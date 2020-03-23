788b5be27952365774dffc73133af8ecf028d607
Mackenzie Hicks <hicks.264@gmail.com>
2020-03-23 13:10:14 -0400

    Updates

---
+++ precache-manifest.d6d438a4ff461dc3bdd96071851ec230.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc78a9246985ec37868326ea2a12073a",
+    "url": "/monsters-roladex/index.html"
+  },
+  {
+    "revision": "e5e0e9d2ece8669e6864",
+    "url": "/monsters-roladex/static/css/main.8103e6db.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-roladex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-roladex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5e0e9d2ece8669e6864",
+    "url": "/monsters-roladex/static/js/main.20dae929.chunk.js"
+  },
+  {
+    "revision": "65a35f1a2e62d3592e5e",
+    "url": "/monsters-roladex/static/js/runtime-main.71d500ff.js"
+  }
+]);
